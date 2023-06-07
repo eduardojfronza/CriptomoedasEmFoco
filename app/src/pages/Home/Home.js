46d4da0259f4297afcc87cd4d048ad09ad6087e4
Eduardo Fronza <edujfronza@gmail.com>
2023-06-07 12:12:05 -0300

    Instalei o React Router e criei a pasta pages

---
+++ app/src/pages/Home/Home.js
@@ -0,0 +1,30 @@
+import './Home.css'
+import Header from '../../components/Header/Header';
+import Sidenav from '../../components/Sidenav/Sidenav';
+import Content from '../../components/Content/Content';   
+
+function Home() {
+    return (
+        <>
+            <section className='container'>
+                <section className='sidenav'>
+                    <Sidenav />
+                </section>
+
+                <section className='header'>
+                    <Header />
+                </section>
+
+                <section className='content '>
+                    <Content />
+                </section>
+
+                <section className='footer'>
+
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default Home
